6ba27f82ecff990a6b4b361d49dc26a10bc38033
José Matheus <XxFULLDLCxX@outlook.com>
2023-07-09 12:17:06 -0300

    Correção na precisão no value de post /transactions

--- src/schemas/transactions.schemas.js
+++ src/schemas/transactions.schemas.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 export const transaction_schema = joi.object({
   description: joi.string().required(),
-  value: joi.number().precision(1).greater(0).required(),
+  value: joi.number().precision(2).greater(0).required(),
   operation: joi.string().valid('input', 'output').required()
 });
 
